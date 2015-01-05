e662942c1fbaf8bbff3ceb503f89dba7fbf41042
JessieSalas <jessie.salas@berkeley.edu>
2015-01-05 14:32:53 -0800

    fixed mail

--- send_email.py
+++ send_email.py
@@ -1,19 +1,25 @@
 import smtplib
-fromaddr = 'patentfoxibm@gmail.com'
-toaddrs  = 'patentfoxibm@gmail.com'
-msg = "\r\n".join([
-  "Subject: Patent Fox Team",
-  "",
-  "Hello email world"
-  ])
-username = 'patentfoxibm@gmail.com'
-password = 'patentfox123'
-server = smtplib.SMTP('smtp.gmail.com:587')
-server.ehlo()
-server.starttls()
-server.login(username,password)
-server.sendmail(fromaddr, toaddrs, msg)
-server.quit()
+
+def send_mail(usr,pss,fromaddr, toaddr, msg):
+    server = smtplib.SMTP('smtp.gmail.com:587')
+    server.ehlo()
+    server.starttls()
+    server.login(username,password)
+    server.sendmail(fromaddr, toaddr, msg)
+    server.quit()
+
+if __name__ == "__main__":
+    fromaddr = 'patentfoxibm@gmail.com'
+    toaddr  = 'oski@berkeley.edu'
+    msg = "\r\n".join([
+      "Subject: Patent Fox Team",
+      "",
+      "Hello email world"
+      ])
+
+    username = 'patentfoxibm@gmail.com'
+    password = 'patentfox123'
+    send_mail(username,password,fromaddr,toaddr,msg)
 
 """
 Allow secure access
